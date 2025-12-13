2e5255120877bc039d5f4b70a292cc0c74cc6f8e
Abdul Kadir <kadirsaifi08@gmail.com>
2025-12-13 12:48:49 +0400

    add read more functionality

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description,imagUrl,newsUrl } = this.props;
+    let { title, description,imagUrl,neswUrl } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn bt-sm btn-primary">
+            <a href={neswUrl} target="_blank" className="btn bt-sm btn-primary">
               Read More
             </a>
           </div>
